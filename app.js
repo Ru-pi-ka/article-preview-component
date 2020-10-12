9a4d82b67f5bba3c38bc8144223c4eaf1f0f17bf
Ru-pi-ka <69966257+Ru-pi-ka@users.noreply.github.com>
2020-10-12 10:37:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const icons=document.querySelector('.share-btn');
+const icons=document.querySelector('.icons-share');
+share.addEventListener('.click',function(){
+    if(icons.style.display=='none'){
+        icons.style.display='block';
+    }else{
+        icons.style.display='none';
+    }
+});
